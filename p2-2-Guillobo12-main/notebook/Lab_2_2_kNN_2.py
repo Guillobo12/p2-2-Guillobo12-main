a7f5d058d9c6088d304a8581765d705cfb8800ae
Guillobo12 <202309412@alu.comillas.edu>
2025-02-13 19:21:58 +0100

    prueba_para_el_print

--- p2-2-Guillobo12-main/notebook/Lab_2_2_kNN_2.py
+++ p2-2-Guillobo12-main/notebook/Lab_2_2_kNN_2.py
@@ -110,6 +110,7 @@ class knn:
             for valor in knn_class_dict.values():
                 lista_resp.append(valor/self.k)
             respuesta.append(lista_resp)
+        print(respuesta)
         respuesta = np.array(respuesta)
         return respuesta
 
